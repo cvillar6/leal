372d63f8543c7b4cd76baf09c74197e063520fe7
Camilo Villa <camilovilla699@gmail.com>
2024-05-01 15:50:36 -0500

    Se añaden propiedades de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        general: {
+          DEFAULT: '#FAFBFF',
+          1: '#161619',
+          2: '#656D76',
+        },
+        info: {
+          DEFAULT: '#175CD3',
+        },
+      },
+      fontFamily: {
+        satoshiLight: ['Satoshi-Light', 'sans-serif'],
+        satoshi: ['Satoshi-Regular', 'sans-serif'],
+        satoshiMedium: ['Satoshi-Medium', 'sans-serif'],
+        satoshiBold: ['Satoshi-Bold', 'sans-serif'],
+      },
+      boxShadow: {
+        DEFAULT: '6px 6px 15px 0px rgba(120, 132, 149, 0.15)',
+      },
+    },
   },
   plugins: [],
 };
