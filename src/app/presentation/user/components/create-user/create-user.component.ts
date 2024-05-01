fed3bc15861c12af3956aceaa6fa4d4d124adf1d
Camilo Villa <camilovilla699@gmail.com>
2024-05-01 18:14:49 -0500

    Se añade sección de crear usuario

--- src/app/presentation/user/components/create-user/create-user.component.ts
+++ src/app/presentation/user/components/create-user/create-user.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.sass']
+  styleUrls: ['./create-user.component.sass'],
 })
 export class CreateUserComponent {
-
+  USERS: string = 'Usuarios';
+  USERS_DESCRIPTION: string =
+    'Aquí puedes crear, editar y eliminar tus usuarios';
+  CREATE: string = 'Crear usuario';
 }
