96af7b15f2d847c9f021a9a2500380e8dcf0ec75
Camilo Villa <camilovilla699@gmail.com>
2024-05-04 13:33:51 -0500

    Se añade footer con enlaces importantest

--- src/app/presentation/shared/components/footer/footer.component.ts
+++ src/app/presentation/shared/components/footer/footer.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.sass'],
 })
 export class FooterComponent {
-
+  REPOSITORY: string = 'Repositorio';
+  STENCIL_REPOSITORY: string = 'Repositorio Stenciljs';
+  NPM_LIBRARY: string = 'Librería Stenciljs';
+  README: string = 'README';
+  LINKEDIN: string = 'LinkedIn';
+  WHATSAPP: string = 'Whatsapp';
 }
